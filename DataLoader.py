2731c5d0ce92464408919e3d4e1bfb50867cb15b
Abdulla Ayyad <abdulla.ayyad96@gmail.com>
2020-09-02 20:53:10 +0400

    Add conv LSTM model

--- DataLoader.py
+++ DataLoader.py
@@ -10,15 +10,15 @@ class DataLoader:
 
     def get_batches_fn_timeseries(self, batch_size):
         # shuffle data
-        ind_list = [i for i in range(len(self.data_['normalized_ref_class']))]
+        ind_list = [i for i in range(len(self.data_['ex_output_equalized']))]
         shuffle(ind_list)
 
-        for batch_i in range(0, len(self.data_['normalized_ref_class']), batch_size):
+        for batch_i in range(0, len(self.data_['ex_output_equalized']), batch_size):
             inputs = []
             outputs = []
-            for i in range(batch_i, np.min([batch_i+batch_size, len(self.data_['normalized_ref_class'])])):
-                inputs.append(self.data_['event_images'][self.data_['normalized_event_image_sequense'][ind_list[i]]])
-                outputs.append(self.data_['normalized_ref_class'][ind_list[i]])
+            for i in range(batch_i, np.min([batch_i+batch_size, len(self.data_['ex_output_equalized'])])):
+                inputs.append(self.data_['event_images'][self.data_['ex_input_image_idx_equalized'][ind_list[i]]])
+                outputs.append(self.data_['ex_output_equalized'][ind_list[i]])
 
 
             yield np.array(inputs), np.array(outputs)
